eee1aa001a5e3fc9de02d153037815915f9b0679
eskr <eskr@universal-robots.com>
2023-08-14 18:07:50 +0200

    load_files

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import harith
 #outputs a final matrix
 def avg_img_prediction(input_image, avg_matrix):
     pass
+
+if __name__ == '__main__':
+
+    training_files = emil.load_training_files_list()

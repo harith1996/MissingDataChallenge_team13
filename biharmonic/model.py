bb6806f13cd4e242ac1ccb43b88d22365b70f553
eskr <eskr@universal-robots.com>
2023-08-15 18:08:56 +0200

    biharmonic

--- biharmonic/model.py
+++ biharmonic/model.py
@@ -2,7 +2,7 @@ import sys
  
 # setting path
 sys.path.append('../MissingDataChallenge_team13')
-
+from skimage.util import img_as_int
 import numpy as np
 import matplotlib.pyplot as plt
 from inpaint_tools import read_file_list
@@ -11,12 +11,12 @@ import argparse
 import pathlib
 import os
 from skimage import io
+import cv2 as cv
 
-from skimage import data
+from PIL import Image
 from skimage.morphology import disk, binary_dilation
 from skimage.restoration import inpaint
 from tqdm import tqdm
-
 
 args = argparse.ArgumentParser(description='InpaintImages')
 config = InPaintConfig(args)
@@ -28,11 +28,6 @@ model_dir = os.path.join(output_data_dir, "trained_model")
 
 inpainted_result_dir = os.path.join(output_data_dir, f"inpainted_{data_set}")
 pathlib.Path(inpainted_result_dir).mkdir(parents=True, exist_ok=True)
-
-# print(f"InPainting {data_set} and placing results in {inpainted_result_dir} with model from {model_dir}")
-
-avg_img_name = os.path.join(model_dir, "average_image.png")
-avg_img = io.imread(avg_img_name)
 
 file_list = os.path.join(input_data_dir, "data_splits", data_set + ".txt")
 file_ids = read_file_list(file_list)
@@ -56,24 +51,10 @@ for idx in tqdm(file_ids):
         mask[tmp_mask] = 1
 
     image_result = inpaint.inpaint_biharmonic(image_defect, mask, channel_axis=-1)
-
-    fig, axes = plt.subplots(ncols=2, nrows=2)
-    ax = axes.ravel()
-
-    ax[0].set_title('Original image')
-    ax[0].imshow(image_orig)
-
-    ax[1].set_title('Mask')
-    ax[1].imshow(mask, cmap=plt.cm.gray)
-
-    ax[2].set_title('Defected image')
-    ax[2].imshow(image_defect)
-
-    ax[3].set_title('Inpainted image')
-    ax[3].imshow(image_result)
-
-    for a in ax:
-        a.axis('off')
-
-    fig.tight_layout()
-    plt.show()
+    io.imshow(image_result)
+    io.show()
+    
+    # image_result = (image_result).astype(np.float32)
+    # print(image_result)
+    io.imsave(out_image_name, image_result)
+    # cv.imwrite(out_image_name, img_as_int(image_result))

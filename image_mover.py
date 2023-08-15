64dc537059c1012f396f3dd334423d07dc628696
Harith Rathish <harith1996@gmail.com>
2023-08-15 15:54:29 +0200

    image moving script

---
+++ image_mover.py
@@ -0,0 +1,13 @@
+#move all training images to a folder
+
+#read data splits text file
+import shutil
+
+
+IMAGE_PATH = "MissingDataOpenData/originals"
+with open('MissingDataOpenData/data_splits/training.txt') as f:
+    content = f.readlines()
+    content = [x.strip() for x in content]
+    print(content)
+    for filename in content:
+        shutil.copy(IMAGE_PATH + "/" + filename +".png", "MissingDataOpenData/training")
